505282ff3f46c5350f40df32ff92a4ee8459cce1
kardashevd <106695001+tsukedev@users.noreply.github.com>
2023-04-09 23:56:06 -0300

    update home/index.tsx

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -23,7 +23,7 @@ import {
 
 export const Home = (): JSX.Element => {
   const gihubUrl = `https://github.com/${userData.githubUser}`;
-  const portfolioUrl = `https://github.com/${userData.githubUser}/my-portfolio`;
+  const portfolioUrl = `https://github.com/${userData.githubUser}/portfolio-tsukedev`;
 
   return (
     <main id="home">
